704460a724c37bfac58e2f3dc0d6aa3ec40d7dec
umar052001 <umar.111nawaz@gmail.com>
2023-08-23 02:11:35 +0500

    added jotai and other necessary folder structure

---
+++ app/OutputPage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OutputPage = () => {
+    return (
+        <div>
+            Output
+        </div>
+    )
+}
+
+export default OutputPage
---
+++ app/utils/context/jotai-context.ts
@@ -0,0 +1,8 @@
+"use client"
+import {atom} from "jotai"
+
+const usernameAtom = atom("");
+
+export {
+    usernameAtom,
+}
